b281f945d79bf8725d3443709010ff9ad8f5ab8f
ssu <74248726+azuressu@users.noreply.github.com>
2025-06-09 21:37:26 +0900

    :white_check_mark: 회원가입 테스트 코드

---
+++ src/test/java/com/auth/presentation/controller/AuthControllerTest.java
@@ -0,0 +1,101 @@
+package com.auth.presentation.controller;
+
+import com.auth.presentation.dto.SignUpRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    void signup_success() throws Exception {
+        // given
+        String username = "username";
+        String nickname = "nickname";
+
+        SignUpRequest request = SignUpRequest.builder()
+                .username(username)
+                .password("secure123!")
+                .nickname(nickname)
+                .build();
+
+        // when & then
+        mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.username").value(username))
+                .andExpect(jsonPath("$.data.nickname").value(nickname))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 테스트_사용자명 중복")
+    void signup_error_duplicate_username() throws Exception {
+        // given
+        String username = "username";
+
+        SignUpRequest request = SignUpRequest.builder()
+                .username(username)
+                .password("secure123!")
+                .nickname("nickname")
+                .build();
+
+        // when
+        mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+
+        // then
+        mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error.code").value("USER_ALREADY_EXISTS"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 테스트_잘못된 입력")
+    void signup_error_empty_field() throws Exception {
+        // given
+        SignUpRequest request = SignUpRequest.builder()
+                .username("")
+                .password("")
+                .nickname("")
+                .build();
+
+        // when & then
+        mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+
+}
