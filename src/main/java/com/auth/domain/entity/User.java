7a58d3548e1f4599021d4afa8fafb6a566ced317
ssu <74248726+azuressu@users.noreply.github.com>
2025-06-09 17:14:04 +0900

    :sparkles: user 도메인 생성

---
+++ src/main/java/com/auth/domain/entity/User.java
@@ -0,0 +1,33 @@
+package com.auth.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+}
---
+++ src/main/java/com/auth/domain/entity/UserRole.java
@@ -0,0 +1,8 @@
+package com.auth.domain.entity;
+
+public enum UserRole {
+
+    USER,    // 사용자
+    ADMIN;   // 관리자
+
+}
